d0bbedce39ddf8be0dcfdbbb7ed59bffeca16b05
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-02-09 20:41:13 -0500

    Create app config file

---
+++ src/config/app.config.ts
@@ -0,0 +1,18 @@
+import { registerAs } from "@nestjs/config";
+
+export default registerAs("app", () => ({
+	// API PORT
+	port: parseInt(process.env.PORT, 10) || 3000,
+
+	// API URL
+	url: process.env.APP_URL || "localhost",
+
+	// API Environment: local | production | staging
+	env: process.env.APP_ENV || "local",
+
+	// API debug mode is enable or not: true | false
+	debugMode: process.env.APP_DEBUG === "false" ? false : true,
+
+	// sites that are CORS enabled
+	frontendURL: process.env.FRONTEND_URL || "localhost"
+}));
