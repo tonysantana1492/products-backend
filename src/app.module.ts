cbc5e837d794a239e9810ce0be2e49a200ab1407
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-02-09 21:11:30 -0500

    Add main configuration to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import appConfig from "./config/app.config";
+import databaseConfig from "./config/database.config";
+import jwtConfig from "./config/jwt.config";
+import { validate } from "./env.validation";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			cache: true,
+			expandVariables: true,
+			validate,
+			load: [appConfig, databaseConfig, jwtConfig]
+		})
+	],
 	controllers: [AppController],
 	providers: [AppService]
 })
