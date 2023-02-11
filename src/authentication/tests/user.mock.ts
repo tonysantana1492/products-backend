d8fe77da7288c471850226b54f471876d98f8c74
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-02-11 16:15:44 -0500

    Create mocks for run tests

---
+++ src/authentication/tests/user.mock.ts
@@ -0,0 +1,17 @@
+import { User } from "src/features/users/entities/user.entity";
+
+export const mockedUserAdmin: User = {
+	_id: "63e7fb74b085b11315705210",
+	name: "John",
+	email: "admin@example.com",
+	password: "hash",
+	role: "admin"
+};
+
+export const mockedUserClient: User = {
+	_id: "63e7fb74b085b11315705210",
+	name: "John",
+	email: "client@example.com",
+	password: "hash",
+	role: "client"
+};
