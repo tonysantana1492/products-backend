9c20faa627bac95889f507e12b57623740025568
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-02-11 16:17:33 -0500

    Create tests for user module

---
+++ src/authentication/tests/authentication.service.integration.spec.ts
@@ -0,0 +1,101 @@
+import { ConfigService } from "@nestjs/config";
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthenticationService } from "../authentication.service";
+import { JwtService } from "@nestjs/jwt";
+import { User } from "../../features/users/entities/user.entity";
+import { UsersService } from "../../features/users/users.service";
+import { mockedJwtService } from "../../utils/mocks/jwt.service";
+import { mockedConfigService } from "../../utils/mocks/config.service";
+import * as bcrypt from "bcrypt";
+import { mockedUserAdmin } from "./user.mock";
+import { getModelToken } from "@nestjs/mongoose";
+
+jest.mock("bcrypt");
+
+describe("The AuthenticationService", () => {
+	let authenticationService: AuthenticationService;
+	let usersService: UsersService;
+	let bcryptCompare: jest.Mock;
+	let userData: User;
+	let findUser: jest.Mock;
+
+	beforeEach(async () => {
+		userData = {
+			...mockedUserAdmin
+		};
+		findUser = jest.fn().mockResolvedValue(userData);
+		const usersRepository = {
+			findOne: findUser
+		};
+
+		bcryptCompare = jest.fn().mockReturnValue(true);
+		(bcrypt.compare as jest.Mock) = bcryptCompare;
+
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				UsersService,
+				AuthenticationService,
+				{
+					provide: ConfigService,
+					useValue: mockedConfigService
+				},
+				{
+					provide: JwtService,
+					useValue: mockedJwtService
+				},
+				{
+					provide: getModelToken(User.name),
+					useValue: usersRepository
+				}
+			]
+		}).compile();
+
+		authenticationService = await module.get<AuthenticationService>(AuthenticationService);
+		usersService = await module.get<UsersService>(UsersService);
+	});
+
+	describe("when accessing the data of authenticating user", () => {
+		it("should attempt to get a user by email", async () => {
+			const getByEmailSpy = jest.spyOn(usersService, "getByEmail");
+			await authenticationService.getAuthenticatedUser("user@email.com", "strongPassword");
+			expect(getByEmailSpy).toBeCalledTimes(1);
+		});
+
+		describe("and the provided password is not valid", () => {
+			beforeEach(() => {
+				bcryptCompare.mockReturnValue(false);
+			});
+			it("should throw an error", async () => {
+				await expect(
+					authenticationService.getAuthenticatedUser("user@email.com", "strongPassword")
+				).rejects.toThrow();
+			});
+		});
+
+		describe("and the provided password is valid", () => {
+			beforeEach(() => {
+				bcryptCompare.mockReturnValue(true);
+			});
+			describe("and the user is found in the database", () => {
+				beforeEach(() => {
+					findUser.mockResolvedValue(userData);
+				});
+				it("should return the user data", async () => {
+					const user = await authenticationService.getAuthenticatedUser("user@email.com", "strongPassword");
+					expect(user).toBe(userData);
+				});
+			});
+
+			describe("and the user is not found in the database", () => {
+				beforeEach(() => {
+					findUser.mockResolvedValue(undefined);
+				});
+				it("should throw an error", async () => {
+					await expect(
+						authenticationService.getAuthenticatedUser("user@email.com", "strongPassword")
+					).rejects.toThrow();
+				});
+			});
+		});
+	});
+});
---
+++ src/authentication/tests/authentication.service.spec.ts
@@ -0,0 +1,43 @@
+import { ConfigService } from "@nestjs/config";
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthenticationService } from "../authentication.service";
+import { JwtService } from "@nestjs/jwt";
+import { User } from "../../features/users/entities/user.entity";
+import { UsersService } from "../../features/users/users.service";
+import { mockedJwtService } from "../../utils/mocks/jwt.service";
+import { mockedConfigService } from "../../utils/mocks/config.service";
+import { getModelToken } from "@nestjs/mongoose";
+
+describe("The AuthenticationService", () => {
+	let authenticationService: AuthenticationService;
+
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [
+				UsersService,
+				AuthenticationService,
+				{
+					provide: ConfigService,
+					useValue: mockedConfigService
+				},
+				{
+					provide: JwtService,
+					useValue: mockedJwtService
+				},
+				{
+					provide: getModelToken(User.name),
+					useValue: {}
+				}
+			]
+		}).compile();
+
+		authenticationService = await module.get<AuthenticationService>(AuthenticationService);
+	});
+
+	describe("when creating a cookie", () => {
+		it("should return a string", () => {
+			const userId = 1;
+			expect(typeof authenticationService.sigInToken({ userId })).toEqual("string");
+		});
+	});
+});
